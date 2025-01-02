5d7c4a6706d171a71451368646d72b2ba2612ce5
bparida99 <biswojitp11@gmail.com>
2025-01-03 03:30:01 +0530

    code added

--- src/main/java/practice/lcodeArray/RemoveDuplicates.java
+++ src/main/java/practice/lcodeArray/RemoveDuplicates.java
@@ -1,9 +1,35 @@
 package practice.lcodeArray;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class RemoveDuplicates {
+
+    public int removeDuplicates2(int[] nums) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] nums2 = new int[nums.length];
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (map.get(nums[i]) == null) {
+                map.put(nums[i], 1);
+                nums2[index] = nums[i];
+                index++;
+            } else {
+                if (map.get(nums[i]) == 1) {
+                    map.put(nums[i], 2);
+                    nums2[index] = nums[i];
+                    index++;
+                }
+            }
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            nums[i] = nums2[i];
+        }
+        return map.entrySet().stream().collect(Collectors.summingInt(Map.Entry::getValue));
+    }
 
     public int removeDuplicates(int[] nums) {
         var result =Arrays.stream(nums).
@@ -14,6 +40,26 @@ public class RemoveDuplicates {
         return result.size();
     }
 
+    public int maxSubArray(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int maxsum = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int currentSum = nums[i];
+            if (currentSum > maxsum) {
+                maxsum = currentSum;
+            }
+            for (int j = i + 1; j < nums.length; j++) {
+                currentSum = currentSum + nums[j];
+                if (currentSum > maxsum) {
+                    maxsum = currentSum;
+                }
+            }
+
+        }
+        return maxsum;
+    }
     public static void main(String arg[]){
 
     }
